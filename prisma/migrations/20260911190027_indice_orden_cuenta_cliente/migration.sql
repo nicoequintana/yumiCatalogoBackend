53d49da96a05e3870053d736dca6a5f5d9f3d037
nicoequintana <nicolasequintana@gmail.com>
2026-09-11 16:02:20 -0300

    fix(cuenta): agrega indice a Orden.cuentaClienteId

---
+++ prisma/migrations/20260911190027_indice_orden_cuenta_cliente/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateIndex
+CREATE NONCLUSTERED INDEX [Orden_cuentaClienteId_idx] ON [dbo].[Orden]([cuentaClienteId]);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
